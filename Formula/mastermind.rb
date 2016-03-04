655da18ac4775f389357f391d36461f844af10bf
Arnau Siches <arnau@ustwo.com>
2016-03-04 15:20:46 +0000

    Fix mastermind artifact path

--- Formula/mastermind.rb
+++ Formula/mastermind.rb
@@ -1,7 +1,7 @@
 class Mastermind < Formula
   desc "Man in the middle testing"
   homepage "https://github.com/ustwo/mastermind"
-  url "https://github.com/ustwo/mastermind/releases/download/v0.9.0.b0/mastermind-v0.9.0-osx-amd64.tar.gz"
+  url "https://github.com/ustwo/mastermind/releases/download/v0.9.0/mastermind-v0.9.0-osx-amd64.tar.gz"
   sha256 "459dca30693a00cc55de4c6f31251a4898644d1edd71105f76f3e4b556662134"
   version "0.9.0"
 
