243f4a765ba3bd384ba80c4e9a479dcafbc73a9d
Arnau Siches <arnau@ustwo.com>
2016-03-21 09:24:08 +0000

    Fix mastermind version

--- Formula/mastermind.rb
+++ Formula/mastermind.rb
@@ -3,7 +3,7 @@ class Mastermind < Formula
   homepage "https://github.com/ustwo/mastermind"
   url "https://github.com/ustwo/mastermind/releases/download/v1.0.0-beta/mastermind-v1.0.0-beta-osx-amd64.tar.gz"
   sha256 "c651350d19f96485f371f80647b43e92bc1a561156368a65df886ad6877eca15"
-  version "0.9.0"
+  version "1.0.0-beta"
 
   def install
     bin.install("mastermind")
