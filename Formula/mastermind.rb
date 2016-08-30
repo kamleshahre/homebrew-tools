a80c303dc9a5fa076fee0afb1e360416420888f3
Arnau Siches <arnau@ustwo.com>
2016-08-30 16:04:37 +0100

    Update mastermind to version 1.0.0

--- Formula/mastermind.rb
+++ Formula/mastermind.rb
@@ -1,9 +1,9 @@
 class Mastermind < Formula
   desc "Man in the middle testing"
   homepage "https://github.com/ustwo/mastermind"
-  url "https://github.com/ustwo/mastermind/releases/download/v1.0.0-beta3/mastermind-v1.0.0-beta3-osx-amd64.tar.gz"
-  sha256 "8b5805a699f4a58d9cc2bc7c35b9f6314981c178ceb9d8ee4a68b36bf62cfce0"
-  version "1.0.0-beta3"
+  version "1.0.0"
+  url "https://github.com/ustwo/mastermind/releases/download/v#{version}/mastermind-v#{version}-osx-amd64.tar.gz"
+  sha256 "7c128cd015c36da68e3b4e32f00e0a34190a80b517f56740822a69b5e649a356"
 
   def install
     bin.install("mastermind")
