9b1e0a8986f8b629527fe2661081c451532a194f
Arnau Siches <arnau@ustwo.com>
2016-12-19 08:08:24 +0000

    Update github-issues to 0.3.0
    Signed-off-by: Arnau Siches <arnau@ustwo.com>

--- Formula/github-issues.rb
+++ Formula/github-issues.rb
@@ -1,9 +1,9 @@
 class GithubIssues < Formula
   desc "Github Issues CLI"
   homepage "https://github.com/ustwo/github-issues"
-  url "https://github.com/ustwo/github-issues/releases/download/v0.2.0/github-issues-v0.2.0-osx-amd64.tar.gz"
-  sha256 "77f6a27ebee3d6256cfb2e42c6f0c1af929d11b607f0659194fcffaea9937591"
-  version "0.2.0"
+  url "https://github.com/ustwo/github-issues/releases/download/v0.3.0/github-issues-v0.3.0-osx-amd64.tar.gz"
+  sha256 "42397cd6f015b1acd6535af70d6460a997876b9133411f0159df7accf34c11fa"
+  version "0.3.0"
 
   def install
     bin.install("github-issues")
